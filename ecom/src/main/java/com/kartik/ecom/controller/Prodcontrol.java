89b771a7b654dfecdc7cb1c6a03157878ded1e92
kartik <kartikthapa249@gmail.com>
2026-09-06 22:19:08 +0530

    Update Prodcontrol.java

--- ecom/src/main/java/com/kartik/ecom/controller/Prodcontrol.java
+++ ecom/src/main/java/com/kartik/ecom/controller/Prodcontrol.java
@@ -3,6 +3,7 @@ package com.kartik.ecom.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.kartik.ecom.service.Productservice;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
+@CrossOrigin
 @RestController
 @RequestMapping("/product")
 public class Prodcontrol {
@@ -26,8 +28,7 @@ public class Prodcontrol {
 	
 //	@GetMapping("/home")
 //	public String kar() {
-//		return service.greet();
-//		
+//		return service.greet();	
 //	}
 	
 	@PostMapping("/multiple")
